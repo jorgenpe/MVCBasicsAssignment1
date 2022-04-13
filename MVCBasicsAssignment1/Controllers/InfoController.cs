1a89806088b7d60ad5866c84611dbfdaecae562f
jorgenpe <82228083+jorgenpe@users.noreply.github.com>
2022-04-13 17:09:59 +0200

    update project

--- MVCBasicsAssignment1/Controllers/InfoController.cs
+++ MVCBasicsAssignment1/Controllers/InfoController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using MVCBasicsAssignment1.Models;
 namespace MVCBasicsAssignment1.Controllers
 {
     public class InfoController : Controller
     {
+        InfoService _infoService;
+
+        public InfoController()
+        {
+            _infoService = new InfoService();
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.Info = _infoService;
             return View();
         }
+
+        [HttpGet]
+        public IActionResult About()
+        {
+            ViewBag.About = _infoService.GetAbout();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            ViewBag.Contact = _infoService.GetContact();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GitHubLinks()
+        {
+            ViewBag.GitHubLinks = _infoService.GetGitHubLinks();
+            return View();
+        }
+
+
     }
-}
+} 
---
+++ MVCBasicsAssignment1/Models/InfoService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace MVCBasicsAssignment1.Models
+{
+    public class InfoService
+    {
+
+        static string about;
+        string[] contact;
+        static List<string[]> gitHubLinks;
+        
+
+        public InfoService()
+        {
+            
+            gitHubLinks = new List<string[]>();
+           
+            about = " Hej! Jag heter Jörgen Pettersson och bor i Mörrum";
+            contact = new string[] { "Namn: Jörgen Pettersson", "Adress: Östravägen 9", " Ort: Mörrum", "Telefon: 0706329725" };
+            string[] gitHubHangman = new string[] { "https://github.com/jorgenpe/Hangman", "Guess a word or character" };
+            string[] gitHubSokoban = new string[] { "https://github.com/jorgenpe/Sokoban", " Push block to green area by using arrow keys" };
+
+            gitHubLinks.Add(gitHubSokoban);
+            gitHubLinks.Add(gitHubHangman);
+        }
+
+        public string GetAbout() { return about; }  
+
+        public string[] GetContact() { return contact;}
+
+        public List<string[]> GetGitHubLinks() { return gitHubLinks;}
+    }
+}
